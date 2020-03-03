e3ed8464ca814d2bfec8144145537fc7752f2fa3
lossp <richard.xxmxx@gmail.com>
2020-03-03 15:06:37 +0800

    model selection and a bug fixed

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # portScanner
 Personal little demo of ports scanning
+
+The main entrance of this little demo is PortScanningController;
+
+This project is based on Maven structure.
+
+There are two models provided
+
+> ​	1.Scan the ports of a certain ip, a max port number is required
+>
+> ​	2.Scan the ip with a certian port provided. More accurate
+
+
+
+There are some tips you might learn.
+
+> Java 8 lambda
+>
+> Java factory model (Use interface)
+>
+> Java Multiple Threads programming
+>
+> Java Socket Programming
+
+
+
+How to run it ? Just execute it.
+
+If you have any better idea, feel free to modify it.
--- src/main/java/portscan/PortScanningController.java
+++ src/main/java/portscan/PortScanningController.java
@@ -9,29 +9,54 @@ import java.util.List;
 public class PortScanningController {
     public static void main(String[] args) {
         try {
-            System.out.println("Please Enter the ip address");
-            InputStream is = System.in;
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String ipAddress = br.readLine();
-            if (!IpAssertion.ipCheck(ipAddress)) throw new IllegalArgumentException("ip地址不合法，请重新输入");
-            System.out.println("请输入最大端口号: ");
-            InputStream is1 = System.in;
-            BufferedReader br1 = new BufferedReader(new InputStreamReader(is1));
-            int initialPortNumber = br1.read();
-            List<PortScanningTask> portTasks = new ArrayList<>();
-            for (int i = 0; i < initialPortNumber; i++) {
-                portTasks.add(new PortScanningTask(i, i, ipAddress));
-            }
-            int threadCount = 10;
-            List[] taskList = Distributor.distributeTask(portTasks, threadCount);
+            System.out.println("Please choose the model");
+            System.out.println("1. Scan all the ports within the range of a certain ip");
+            System.out.println("2. Scan the certain ip and its certain port");
+            InputStream initialInput = System.in;
+            BufferedReader initialInputBufferReader = new BufferedReader(new InputStreamReader(initialInput));
+            int model = Integer.parseInt(initialInputBufferReader.readLine());
+            if (model == 1) {
+                System.out.println("Please Enter the ip address");
+                InputStream is = System.in;
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String ipAddress = br.readLine();
+                if (!IpAssertion.ipCheck(ipAddress)) throw new IllegalArgumentException("ip地址不合法，请重新输入");
+                System.out.println("请输入最大端口号: ");
+                InputStream is1 = System.in;
+                BufferedReader br1 = new BufferedReader(new InputStreamReader(is1));
+                int initialPortNumber = Integer.parseInt(br1.readLine());
+                List<PortScanningTask> portTasks = new ArrayList<>();
+                for (int i = 0; i < initialPortNumber; i++) {
+                    System.out.println("基本");
+                    portTasks.add(new PortScanningTask(i, i, ipAddress));
+                }
+                int threadCount = 10;
+                List[] taskList = Distributor.distributeTask(portTasks, threadCount);
 
-            PortScanningFactory.getPortScanningThread<PortScanningThread> factory = () -> new PortScanningThread();
-            for (int i = 0; i < threadCount; i++) {
-                //初始化线程
+                PortScanningFactory.getPortScanningThread<PortScanningThread> factory = () -> new PortScanningThread();
+                for (int i = 0; i < taskList.length; i++) {
+                    //初始化线程
+                    PortScanningThread portScanningThread = factory.newInstance();
+                    portScanningThread.setTasks(taskList[i]);
+                    portScanningThread.setThreadId(i);
+                    Thread thread = new Thread(portScanningThread, "thread = " + i);
+                    thread.start();
+
+                }
+            } else if (model == 2) {
+                System.out.println("Please enter the ip");
+                BufferedReader ipBufferReader = new BufferedReader(new InputStreamReader(System.in));
+                String ipAddress = ipBufferReader.readLine();
+                System.out.println("Please enter the port");
+                BufferedReader portBufferReader = new BufferedReader(new InputStreamReader(System.in));
+                int port = Integer.parseInt(portBufferReader.readLine());
+                PortScanningFactory.getPortScanningThread<PortScanningThread> factory = () -> new PortScanningThread();
                 PortScanningThread portScanningThread = factory.newInstance();
-                portScanningThread.setTasks(taskList[i]);
-                portScanningThread.setThreadId(i);
-                Thread thread = new Thread(portScanningThread, "thread = " + i);
+                List<PortScanningTask> portTasks = new ArrayList<>();
+                portTasks.add(new PortScanningTask(1, port, ipAddress));
+                portScanningThread.setTasks(portTasks);
+                portScanningThread.setThreadId(1);
+                Thread thread = new Thread(portScanningThread, "thread = 1");
                 thread.start();
 
             }
